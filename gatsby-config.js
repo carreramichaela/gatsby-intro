7bfe659f11f7b56505b5d1d0c1fda1a326414ff0
Michaela Carrera <carrera.michaela104@gmail.com>
2019-10-14 09:09:28 -0400

    built a Gatsby website

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,12 +7,17 @@ module.exports = {
   plugins: [
     'gatsby-plugin-emotion',
     'gatsby-plugin-react-helmet',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-plugin-mdx',
       options: {
         defaultLayouts: {
           default: require.resolve('./src/components/layout.js'),
         },
+        gatsbyRemarkPlugins: [{
+          resolve: 'gatsby-remark-images'
+        }]
       },
     },
     {
@@ -22,5 +27,28 @@ module.exports = {
         path: 'posts',
       },
     },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: 'images',
+      },
+    },
+    {
+      resolve: `gatsby-source-instagram`,
+      options: {
+        username: `lapatronabakery`,
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-webpack-bundle-analyzer',
+      options: {
+        production: true,
+        disable: !process.env.ANALYZE_BUNDLE_SIZE,
+        generateStatsFile: true,
+        analyzerMode: 'static',
+      }
+    }
+
   ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import Layout from '../components/layout';
+import Hero from '../components/hero';
+import Insta from '../components/insta'
 import usePosts from '../hooks/use-posts';
 import PostPreview from '../components/post-preview';
-import ReadLink from '../components/read-link';
-
 export default () => {
   const posts = usePosts();
-  return (
-    <Layout>
-      <h1>Home</h1>
-      <p>Hello Minnesota!</p>
-      <ReadLink to="/about/">Learn about me &rarr;</ReadLink>
 
+  return (
+    <>
+    <Hero/>
+    <Layout>
       <h2>Read my blog</h2>
       {posts.map(post => (
         <PostPreview key={post.slug} post={post} />
       ))}
+    
+    <Insta />
+
     </Layout>
+    </>
   );
 };
